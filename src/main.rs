474bd5d4195c52fb3d58028f56c24ce41cd2a3da
sonro <sonro@gmx.com>
2017-10-30 15:42:31 +0000

    Clean up main function by outsourcing it's work
    Create functions to deal with:
        - getting user input
        - converting tempreture based on selection
        - selecting tempreture to convert to
    
    Also removed uneccesary comments.
    Also created `Convertor` type to contain relevent function.

--- src/main.rs
+++ src/main.rs
@@ -1,183 +1,97 @@
-//a simple temperature unit converter
 use std::io;
-use std::str;
-//program
+
+struct Convertor {
+    scale: String,
+    function: fn(f64) -> f64,
+}
+
 fn main() {
     loop {
-        //introduces the user to their options for unit_type
-        println!("Select the unit you would like to convert from:", );
-        println!("Type 1 for Fahrenheit, 2 for Celsius, or 3 for Kelvin", );
-        //allows user input
-        let mut unit = String::new();
-        //reads user input
-        io::stdin().read_line(&mut unit)
-            .expect("Failed to read line");
-        let unit = unit.trim();
-        //states units used
-        match unit {
-            //F
-            "1" => {
-                println!("You are converting from Fahrenheit", );
-                //input unit to convert to
-                println!("Input what you would like to convert to:", );
-                println!("Type 1 for Celsius or 2 for Kelvin", );
-                let mut to_unit = String::new();
-                io::stdin().read_line(&mut to_unit)
-                    .expect("Failed to read line");
-                let to_unit = to_unit.trim();
-                //determines what equation to use
-                match to_unit {
-                    //F=>C
-                    "1" => {
-                        println!("You are converting to Celsius", );
-                        //input
-                        println!("Input the temperature you have", );
-                        let mut v = String::new();
-                        io::stdin().read_line(&mut v)
-                            .expect("Failed to read line");
-                        //math
-                        let v = v.trim().parse::<f32>().expect("That's not a number!");
-                        let v = v - 32.0;
-                        let v = v * 5.0 / 9.0;
-                        println!("This is equal to {} degrees in Celsius
-                        ", v);
-                    }
-                    //F=>K
-                    "2" => {
-                        println!("You are converting to Kelvin", );
-                        //input
-                        println!("Input the temperature you have", );
-                        let mut v = String::new();
-                        io::stdin().read_line(&mut v)
-                            .expect("Failed to read line");
-                        //math
-                        let v = v.trim().parse::<f32>().expect("That's not a number!");
-                        let v = v + 459.67;
-                        let v = v * 5.0 / 9.0;
-                        //says result
-                        println!("This is equal to {} degrees in Kelvin
-                        ", v);
-                    }
-                    _ => {
-                        println!("Invalid choice", );
-                    }
-                }
-            }
-            //C
-            "2" => {
-                println!("You are converting from Celsius", );
-                //input unit to convert to
-                println!("Type 1 for Fahrenheit or type 2 for Kelvin:", );
-                let mut to_unit = String::new();
-                io::stdin().read_line(&mut to_unit)
-                    .expect("Failed to read line");
-                let to_unit = to_unit.trim();
-                //determines which equation to use
-                match to_unit {
-                    //C=>F
-                    "1" => {
-                        println!("You are converting to Fahrenheit", );
-                        //input
-                        println!("Input the temperature you have", );
-                        let mut v = String::new();
-                        io::stdin().read_line(&mut v)
-                            .expect("Failed to read line");
-                        //math
-                        let v = v.trim().parse::<f32>().expect("That's not a number!");
-                        let v = v * 18.0 / 10.0;
-                        let v = v + 32.0;
-                        println!("This is equal to {} degrees in Fahrenheit
-                        ", v);
-                    }
-                    //C=>K
-                    "2"=> {
-                        println!("You are converting to Kelvin", );
-                        //input
-                        println!("Input the temperature you have", );
-                        let mut v = String::new();
-                        io::stdin().read_line(&mut v)
-                            .expect("Failed to read line");
-                        //math
-                        let v = v.trim().parse::<f32>().expect("That's not a number!");
-                        let v = v + 273.15;
-                        //says result
-                        println!("This is equal to {} degrees in Kelvin
-                        ", v);
-                    }
-                    _ => {
-                        println!("Invalid choice", );
-                    }
-                }
-            }
-            //K
-            "3" => {
-                println!("You are converting from Kelvin", );
-                //input unit to convert to
-                println!("Input what you would like to convert to:", );
-                println!("Type 1 for Fahrenheit or 2 for Celsius", );
-                let mut to_unit = String::new();
-                io::stdin().read_line(&mut to_unit)
-                    .expect("Failed to read line");
-                let to_unit = to_unit.trim();
-                //determines which equation to use
-                match to_unit {
-                    //K=>F
-                    "1" => {
-                        println!("You are converting to Fahrenheit", );
-                        //input
-                        println!("Input the temperature you have", );
-                        let mut v = String::new();
-                        io::stdin().read_line(&mut v)
-                            .expect("Failed to read line");
-                        //math
-                        let v = v.trim().parse::<f32>().expect("That's not a number!");
-                        let v = v * 18.0 / 10.0;
-                        let v = v - 459.67;
-                        println!("This is equal to {} degrees in Fahrenheit
-                        ", v);
-                    }
-                    //K=>C
-                    "2" => {
-                        println!("You are converting to Celsius", );
-                        //input
-                        println!("Input the temperature you have", );
-                        let mut v = String::new();
-                        io::stdin().read_line(&mut v)
-                            .expect("Failed to read line");
-                        //math
-                        let v = v.trim().parse::<f32>().expect("That's not a number!");
-                        let v = v - 273.15;
-                        //says result
-                        println!("This is equal to {} degrees in Celsius
-                        ", v);
-                    }
-                    _ => {
-                        println!("Invalid choice", );
-                    }
-                }
-            }
-            _ => {
-                println!("Invalid choice", );
-            }
+        let prompt = String::from(
+            "Select the unit you would like to convert from:\nType 1 for Fahrenheit, 2 for Celsius, or 3 for Kelvin"
+            );
+        match get_user_input::<i32>(&prompt) {
+            1 => convert_choice("Fahrenheit", 
+                                Convertor {
+                                    scale: String::from("Celisus"),
+                                    function: |x: f64| (x - 32.0) * 5.0 / 9.0,
+                                }, Convertor {
+                                    scale: String::from("Kelvin"),
+                                    function: |x: f64| (x + 459.67) * 5.0 / 9.0,
+                                }),
+
+            2 => convert_choice("Celsius", 
+                                Convertor {
+                                    scale: String::from("Fahrenheit"),
+                                    function: |x: f64| (x * 18.0 / 10.0) + 32.0,
+                                }, Convertor {
+                                    scale: String::from("Kelvin"),
+                                    function: |x: f64| x + 273.15,
+                                }),
+            3 => convert_choice("Kelvin",
+                                Convertor {
+                                    scale: String::from("Fahrenheit"),
+                                    function: |x: f64| (x * 18.0 / 10.0) - 459.67,
+                                }, Convertor {
+                                    scale: String::from("Celisus"),
+                                    function: |x: f64| x - 273.15,
+                                }),
+            _ => println!("Invalid Choice"),
         }
-        //asks if another conversion is required
-        println!("
-        Do you have another temperature to convert?
-        y=Yes, n=No", );
-        let mut ask = String::new();
-        io::stdin().read_line(&mut ask)
-            .expect("Failed to read line");
-        let ask = ask.trim();
-        match ask {
-            "y" => {
-                println!("Ok then.", );
-            }
-            "n" => {
-                break;
-            }
-            _ => {
-                println!("Invalid choice", );
-            }
+
+        let prompt = String::from(
+            "Do you have another temperature to convert?\ny=Yes, n=No"
+            );
+        match get_user_string(&prompt).trim() {
+            "y" => println!("Ok then."),
+            "n" => break,
+            _ => println!("Invalid choice"),
         }
+    }
+}
+
+fn convert(input: Convertor) {
+    let prompt = format!(
+        "You are converting to {}\nInput the temperature you have", input.scale
+        );
+    let temp = get_user_input::<f64>(&prompt);
+    println!("This is equal to {:.2} degrees in {}", (input.function)(temp), input.scale);
+}
+
+fn convert_choice(from_scale: &str, option_1: Convertor, option_2: Convertor) {
+    loop {
+        let prompt = format!(
+            "You are converting from {}\nInput what you would like to convert to:\nType 1 for {} or 2 for {}",
+            from_scale,
+            option_1.scale,
+            option_2.scale
+            );
+        match get_user_input::<i32>(&prompt) {
+            1 => convert(option_1),
+            2 => convert(option_2),
+            _ => {
+                println!("Invalid choice");
+                continue;
+            },
+        }
+        break;
+    }
+}
+
+fn get_user_string(prompt: &str) -> String {
+    let mut input = String::new();
+    println!("{}", prompt);
+    io::stdin().read_line(&mut input).expect("Reading stdin");
+    println!();
+    input
+}
+
+fn get_user_input<T: std::str::FromStr>(prompt: &str) -> T {
+    loop {
+        let input = get_user_string(prompt);
+        match input.trim().parse::<T>() {
+            Ok(n) => return n,
+            _ => println!("Invalid Input"),
+        };
     }
 }
